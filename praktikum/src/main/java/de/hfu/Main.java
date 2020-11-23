311f6d3065911fd89dfef59240027b86cc59b4c9
Steffen-osse <73649744+Steffen-osse@users.noreply.github.com>
2020-11-23 10:30:00 +0100

    Erster Commit

---
+++ praktikum/src/main/java/de/hfu/Main.java
@@ -0,0 +1,18 @@
+package de.hfu;
+
+import java.util.Scanner;
+import java.io.*;
+
+//Hallo das ist ein Kommentar!
+
+    public class Main {
+
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+            System.out.println(input.toUpperCase());
+        }
+
+    }
+
+
